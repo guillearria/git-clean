4ac214f5e59b16d5f1a1c1954bc1f8b27ff44373
sapinspys <garri009@fiu.edu>
2020-07-19 00:33:46 -0400

    Main index page populated and styled (basic)

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Layout, Button } from "antd";
+import { GithubFilled, GithubOutlined } from "@ant-design/icons";
+
+import "./App.css";
+
+const { Header, Footer, Content } = Layout;
 
 function App() {
   const [status, setStatus] = useState();
   const [token, setToken] = useState(null);
 
   return (
-    <div className="App">
-    </div>
+    <Layout className="App">
+      <Header className="App-header">
+        <h1>GitClean</h1>
+      </Header>
+      <Content className="App-content">
+        <h1>Simple GitHub organization. Powerful results.</h1>
+        <p>
+          Easily organize your GitHub profile by storing or deleting old
+          repositories.
+        </p>
+        <Button type="primary" shape="round" icon={<GithubOutlined />}>
+          TRY NOW
+        </Button>
+      </Content>
+      <Footer className="App-footer">
+        <a href="https://github.com/arriadevoe/git-clean">
+          <GithubFilled />
+        </a>
+      </Footer>
+    </Layout>
   );
 }
 
