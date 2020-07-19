ad7c72d032a191de71b6631a9207640eadf69445
sapinspys <garri009@fiu.edu>
2020-07-19 00:54:05 -0400

    Added GH authorization link

--- app/src/App.js
+++ app/src/App.js
@@ -5,9 +5,10 @@ import { GithubFilled, GithubOutlined } from "@ant-design/icons";
 import "./App.css";
 
 const { Header, Footer, Content } = Layout;
+const CLIENT_ID = "7fe013c90e0777329367";
+const REDIRECT_URI = "http://localhost:3000/";
 
 function App() {
-  const [status, setStatus] = useState();
   const [token, setToken] = useState(null);
 
   return (
@@ -21,9 +22,13 @@ function App() {
           Easily organize your GitHub profile by storing or deleting old
           repositories.
         </p>
-        <Button type="primary" shape="round" icon={<GithubOutlined />}>
-          TRY NOW
-        </Button>
+        <a
+          href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+        >
+          <Button type="primary" shape="round" icon={<GithubOutlined />}>
+            TRY NOW
+          </Button>
+        </a>
       </Content>
       <Footer className="App-footer">
         <a href="https://github.com/arriadevoe/git-clean">
