332331c3c8bcee4476b4a53a4d703f98cd9d970e
sapinspys <garri009@fiu.edu>
2020-07-20 17:54:00 -0400

    Implemented basic GitHub API functionality

--- app/src/screens/Dashboard.js
+++ app/src/screens/Dashboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import GitHub from "github-api";
 import { Layout } from "antd";
 
 import "./LandingScreen.css";
@@ -6,10 +7,39 @@ import "./LandingScreen.css";
 const { Content } = Layout;
 
 function LandingScreen(props) {
+  const [userData, setUserData] = useState({});
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    let gh = new GitHub({
+      token: props.token,
+    });
+
+    let auth_user = gh.getUser();
+
+    auth_user.getProfile((err, profile) => {
+      console.log(profile);
+      setUserData(profile);
+    });
+
+    auth_user.listRepos((err, repos) => {
+      console.log(repos);
+      setRepos(repos);
+    });
+  }, []);
+
   return (
     <Content>
-      <h1>GitHub access authorized.</h1>
-      <p>Token: {props.token}</p>
+      <h1>Welcome {userData.login}</h1>
+      <img src={userData.avatar_url} alt="avatar" />
+      <div>
+        <h2>Your repos:</h2>
+        <ul>
+          {repos.map((repo) => (
+            <li>{repo.full_name}</li>
+          ))}
+        </ul>
+      </div>
     </Content>
   );
 }
