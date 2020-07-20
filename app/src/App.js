46e86faa4b84d928592784a01cafa9a7b5c31b32
sapinspys <garri009@fiu.edu>
2020-07-19 23:49:27 -0400

    Set up basic SPA logic

--- app/src/App.js
+++ app/src/App.js
@@ -9,7 +9,7 @@ const CLIENT_ID = "7fe013c90e0777329367";
 const REDIRECT_URI = "http://localhost:3000/";
 
 function App() {
-  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState("unauthorized");
   const [token, setToken] = useState(null);
 
   useEffect(() => {
@@ -17,35 +17,48 @@ function App() {
       window.location.href.match(/?code=(.*)/) &&
       window.location.href.match(/?code=(.*)/)[1];
     if (code) {
-      setLoading(true);
+      setStatus("loading"); // ??
       fetch(`http://git-clean.herokuapp.com/authenticate/${code}`)
         .then((res) => res.json())
         .then((token) => {
           setToken(token);
-          setLoading(false);
+          setStatus("authorized");
         });
     }
   }, []);
+
+  let content = (
+    <Content className="App-content">
+      <h1>Simple GitHub organization. Powerful results.</h1>
+      <p>
+        Easily organize your GitHub profile by storing or deleting old
+        repositories.
+      </p>
+      <a
+        href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+      >
+        <Button type="primary" shape="round" icon={<GithubOutlined />}>
+          TRY NOW
+        </Button>
+      </a>
+    </Content>
+  );
+
+  if (token && status == "authorized") {
+    content = (
+      <Content>
+        <h1>GitHub access authorized.</h1>
+        <p>Token: {token}</p>
+      </Content>
+    );
+  }
 
   return (
     <Layout className="App">
       <Header className="App-header">
         <h1>GitClean</h1>
       </Header>
-      <Content className="App-content">
-        <h1>Simple GitHub organization. Powerful results.</h1>
-        <p>
-          Easily organize your GitHub profile by storing or deleting old
-          repositories.
-        </p>
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
-        >
-          <Button type="primary" shape="round" icon={<GithubOutlined />}>
-            TRY NOW
-          </Button>
-        </a>
-      </Content>
+      {content}
       <Footer className="App-footer">
         <a href="https://github.com/arriadevoe/git-clean">
           <GithubFilled />
